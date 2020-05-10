cb6e9541b014550515afecd84c3a431481536164
Shantanu <shantanumittal3499@gmail.com>
2020-05-10 21:42:21 +0530

    Update clean.py
    Updated various cleaning functions

--- clean.py
+++ clean.py
@@ -8,6 +8,7 @@ Created on Wed Apr 22 13:09:54 2020
 
 import pandas as pd
 import numpy as np
+from sklearn.impute._iterative import IterativeImputer
 from sklearn.preprocessing import RobustScaler, StandardScaler, MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
@@ -16,7 +17,7 @@ from sklearn.linear_model import LassoCV
 from sklearn.ensemble import RandomForestRegressor
 
 
-def cleaning(data,y):
+def cleaning(data,y,params):
     size = len(data)
     ids = ["ID","id","Id","iD"]
     def isId(i):
@@ -30,7 +31,9 @@ def cleaning(data,y):
     for i in data.columns:
         if (isId(i)) or (data[i].dtype == 'O' and len(data[i].value_counts())>300):
             drp.add(i)
-    
+
+    params["nullcount"] = sum(data.isnull().sum()>0)
+
     for i in data.columns:
         if data[i].isnull().sum()>=size//2:
             drp.add(i)
@@ -55,32 +58,26 @@ def cleaning(data,y):
     
     print("Filling NULL values...")
     for i in cat_col:
-        m = data[i].mode()
-        data[i] = data[i].fillna(m)
-        
-    for i in num_col:
-        m = data[i].mean()
-        data[i] = data[i].fillna(m)
+        data[i] = data[i].fillna("Missing")
+
+    imp = IterativeImputer(random_state=123)
+    data[num_col] = pd.DataFrame(imp.fit_transform(data[num_col]), columns=num_col)
     print("NULL Values removed!")
-    print("Checkiing is any null values are left: ",any(data.isnull().sum()>0))
+    print("Checking is any null values are left: ",any(data.isnull().sum()>0))
     
     print("="*100)
     
     if len(cat_col)!=0:
+        for i in cat_col:
+            temp = data.groupby(i).size() / size
+            data.loc[:, i + '_val'] = data[i].map(temp)
+            data.loc[data[i + '_val'] <= 0.01, i] = 'Rare'
+            data.drop(i + '_val', axis=1, inplace=True)
+
         print("Creating dummy variables...")
-        cat_features = pd.get_dummies(data[cat_col],drop_first=True)
-        print("Final shape of categorical features: ",cat_features.shape)
-    
-    if len(num_col)!=0:
-        num_features = data[num_col]
-        print("Final shape of numerical features: ",num_features.shape)
-    
-    if len(cat_col)==0:
-        data = num_features
-    elif len(num_col) == 0:
-        data = cat_features
-    else:
-        data = pd.concat([num_features,cat_features],axis=1)
+        data = pd.get_dummies(data,drop_first=True)
+
+
     print("Final shape of dataset: ",data.shape)
     
     print("="*100)
@@ -102,7 +99,25 @@ def cleaning(data,y):
     selector1.fit(data_scaled,y)
     selector1.get_support()
     feat_selected1 = list(data.columns[(selector1.get_support())])
-    
+
+    feat_imp = set()
+    temp1 = list((selector1.estimator_.coef_)[(selector1.get_support())])
+    mx = max(temp1)
+    mn = min(temp1)
+
+    def transform(x):
+        return (x - mn) / (mx - mn)
+
+    for i in range(len(temp1)):
+        feat_imp.add((feat_selected1[i], round(transform(abs(temp1[i]))*100,2)))
+    feat_imp = list(feat_imp)
+    feat_imp.sort(key=lambda x: x[1], reverse=True)
+    feat_show = feat_imp[:min(5, len(feat_imp))]
+    feat_show[-1] = (feat_show[-1][0], feat_show[-1][1]+1)
+    params["features"] = feat_show
+
+    print(params["features"])
+
     selector2 = RandomForestRegressor()
     selector2 = SelectFromModel(selector2)
     selector2.fit(data_scaled,y)
@@ -111,10 +126,12 @@ def cleaning(data,y):
     feat_selected = list(set(feat_selected1+feat_selected2))
     
     print("Found {} important features".format(len(feat_selected)))
-    
+
+    params["feat_count"] = len(feat_selected)
+
     data_feat = data[feat_selected]
     print("final shape of dataset",data_feat.shape)
     
     print("="*100)
     
-    return data_feat,y
+    return data_feat,y,params
